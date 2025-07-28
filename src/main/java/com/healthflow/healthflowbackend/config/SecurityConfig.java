c464601dd8a09972f7bbd62ddc8e6e572d97b7a7
Eric Hao <hkf1239948@163.com>
2025-07-28 14:28:32 +0100

    feat: reconfigure environment variables

--- src/main/java/com/healthflow/healthflowbackend/config/SecurityConfig.java
+++ src/main/java/com/healthflow/healthflowbackend/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:3000"));
+        config.setAllowedOriginPatterns(List.of("https://healthflow-frontend-sl57.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
